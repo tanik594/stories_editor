5a2d113519227766a03a918e3eaa311e6ba069eb
intellign <ismailarashad@aol.com>
2023-09-19 11:19:28 -0400

    Update painting.dart

--- lib/src/presentation/painting_view/painting.dart
+++ lib/src/presentation/painting_view/painting.dart
@@ -174,7 +174,7 @@ class _PaintingState extends State<Painting> {
                 const Align(
                   alignment: Alignment.centerLeft,
                   child: Padding(
-                    padding: EdgeInsets.only(bottom: 140),
+                    padding: EdgeInsets.only(bottom:  0), //140
                     child: SizeSliderWidget(),
                   ),
                 ),
